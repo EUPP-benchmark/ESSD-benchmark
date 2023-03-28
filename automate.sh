a02de31022aba9cee58667d73883e93cc43a9e76
Jonathan Demaeyer <jodemaey@meteo.be>
2023-03-28 11:36:32 +0200

    Continued automation script

--- automate.sh
+++ automate.sh
@@ -6,5 +6,7 @@ git submodule update
 cd datasets/ESSD-benchmark-datasets/
 conda env create -f environment.yml
 bash -i download_ESSD_dataset.sh
-
+cd ../..
+mkdir data
+mv ./datasets/ESSD-benchmark-datasets/*.nc ./data/
 

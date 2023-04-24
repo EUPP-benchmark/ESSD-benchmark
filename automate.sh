da6c2947ce4b8102a08a22fe812231af931af017
Jonathan Demaeyer <jodemaey@meteo.be>
2023-04-24 10:46:59 +0200

    Added DRN method to automation script

--- automate.sh
+++ automate.sh
@@ -5,7 +5,7 @@ git submodule update
 # creating the dataset
 cd datasets/ESSD-benchmark-datasets/
 conda env create -f environment.yml
-bash -i download_ESSD_dataset.sh
+#bash -i download_ESSD_dataset.sh
 cd ../..
 mkdir data
 mv ./datasets/ESSD-benchmark-datasets/*.nc ./data/
@@ -38,6 +38,7 @@ echo "ASRE method done !"
 # 3 - ANET (done with the MBM env)
 echo "Postprocessing with the ANET method"
 cd ../ESSD-ANET
+rm -rf Model_*
 conda install pytorch -c conda-forge -y
 python train.py ../../data/
 python generate.py ../../data/ `ls -d Model_*`
@@ -46,9 +47,25 @@ echo "ANET method done !"
 # 3 - DRN (done with the MBM env)
 echo "Postprocessing with the DRN method"
 cd ../ESSD-DRN
-conda install pyarrow -c conda-forge -y
+mkdir data-feather
+mkdir data-csv
+mkdir data-nc
+conda deactivate
+conda env create -f environment.yml
+conda activate ESSD-DRN
 ln -s ../../data EUPPdata
 cd data-preprocess
-
-
+python test_netCDF_to_feather.py
+python train_netCDF_to_feather.py
+python data_preprocess.py
+cd ..
+python DRN_pp.py
+cd ..
+cd output-process
+python output_process.py
+Rscript install.R
+Rscript generate_fcst.R
+python feather_to_netCDF.py
+cd ..
+echo "DRN method done !"
 
